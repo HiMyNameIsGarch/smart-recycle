1f7edff40374ebbb40c9e427b11cad27c41cb5cc
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2021-12-03 16:32:48 +0200

    add firebase to main application

--- app/src/main/java/com/example/smart_recycle/MainActivity.java
+++ app/src/main/java/com/example/smart_recycle/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.smart_recycle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 
 public class MainActivity extends AppCompatActivity {
     Button btn1;
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
         setContentView(R.layout.activity_main);
         btn1=(Button)findViewById(R.id.button1);
         txt1=(TextView)findViewById(R.id.textView1);
