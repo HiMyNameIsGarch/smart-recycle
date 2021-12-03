2e5d28d29e2b6ad1211917f170b75f30082fc6a8
PatrickGM1 <63743546+PatrickGM1@users.noreply.github.com>
2021-12-03 20:47:42 +0200

    Skip button

--- app/src/main/java/com/example/smart_recycle/MainActivity.java
+++ app/src/main/java/com/example/smart_recycle/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.smart_recycle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,29 +8,21 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
-
 public class MainActivity extends AppCompatActivity {
-    Button btn1;
-    TextView txt1;
+    Button login, register, skip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
         setContentView(R.layout.activity_main);
-        btn1=(Button)findViewById(R.id.button1);
-        txt1=(TextView)findViewById(R.id.textView1);
-        btn1.setOnClickListener(new View.OnClickListener() {
+        login = (Button)findViewById(R.id.button1);
+        register = (Button)findViewById(R.id.button);
+        skip = (Button)findViewById(R.id.button2);
+        skip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Write a message to the database
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("test");
-                myRef.setValue("Hello, World!");
-                txt1.setText("Salut");
+                startActivity(new Intent(MainActivity.this, MainContent.class));
             }
         });
+
     }
 }
