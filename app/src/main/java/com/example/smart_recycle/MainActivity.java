a3fca6ca98806a869763b74c3fbca345d0e3cf49
PatrickGM1 <63743546+PatrickGM1@users.noreply.github.com>
2021-12-03 17:34:43 +0200

    Buton de test

--- app/src/main/java/com/example/smart_recycle/MainActivity.java
+++ app/src/main/java/com/example/smart_recycle/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.smart_recycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn1;
+    TextView txt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=(Button)findViewById(R.id.button1);
+        txt1=(TextView)findViewById(R.id.textView1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt1.setText("Salut");
+            }
+        });
+
+
     }
+
 }
