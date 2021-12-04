1819e2d56940f4199c95501e659b5e90d6960115
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2021-12-04 10:44:36 +0200

    remove dead loop

--- app/src/main/java/com/example/smart_recycle/MapsActivity.java
+++ app/src/main/java/com/example/smart_recycle/MapsActivity.java
@@ -38,10 +38,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        for(GarbageMarker gm: garbageMarkers) {
-            System.out.println(gm.Name);
-        }
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
