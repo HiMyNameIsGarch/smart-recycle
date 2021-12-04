291d26f19b9b96cebf6a8d254fa84e53e6584113
PatrickGM1 <63743546+PatrickGM1@users.noreply.github.com>
2021-12-04 19:53:04 +0200

    Added a top users list

--- app/src/main/java/com/example/smart_recycle/Top_garbage_collectors.java
+++ app/src/main/java/com/example/smart_recycle/Top_garbage_collectors.java
@@ -5,6 +5,7 @@ import static com.google.android.gms.tasks.Tasks.await;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,26 +20,33 @@ import java.util.List;
 
 public class Top_garbage_collectors extends AppCompatActivity {
     private DatabaseReference dbUsers = FirebaseDatabase.getInstance().getReference();
-    private Query query = dbUsers.child("Users").orderByChild("garbagePoints");
+    private Query query = dbUsers.child("Users").orderByChild("garbagePoints").limitToFirst(10);
+    public TextView lista;
+    String listastring="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_garbage_collectors);
+        lista=(TextView)findViewById(R.id.listatop);
+        lista.setText(listastring);
         DisplayTopUsers();
     }
     private void DisplayTopUsers() {
+
         List<User> topUsers = new ArrayList<User>();
         ValueEventListener eventListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                long i = dataSnapshot.getChildrenCount();
+                if(i>10){
+                    i=10;
+                }
                 for(DataSnapshot ds : dataSnapshot.getChildren()) {
                     System.out.println(ds.getValue(User.class).name);
-                    topUsers.add(ds.getValue(User.class));
-                }
-                // Sort the users
-                //Collections.sort(topUsers, new Sortbypoints());
-                for(User u: topUsers){
-                    // Display all the users in app
+                    User user = ds.getValue(User.class);
+                    listastring = i+". "+ user.name +": "+ user.garbagePoints+ "\n"+listastring;
+                    i--;
+                    lista.setText(listastring);
                 }
             }
             @Override
