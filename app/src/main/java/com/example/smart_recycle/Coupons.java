02ca2fcac18c43fd8a4888d33fd50ad07c5dfe3a
PatrickGM1 <63743546+PatrickGM1@users.noreply.github.com>
2021-12-04 21:14:41 +0200

    Added coupons section

---
+++ app/src/main/java/com/example/smart_recycle/Coupons.java
@@ -0,0 +1,13 @@
+package com.example.smart_recycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Coupons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coupons);
+    }
+}
