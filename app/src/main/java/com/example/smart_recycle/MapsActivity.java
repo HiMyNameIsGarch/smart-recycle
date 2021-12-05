1a60b7f90db57593099a8a4b7ce992a62e1be62c
PatrickGM1 <63743546+PatrickGM1@users.noreply.github.com>
2021-12-05 10:52:03 +0200

    Font bug

--- app/src/main/java/com/example/smart_recycle/MapsActivity.java
+++ app/src/main/java/com/example/smart_recycle/MapsActivity.java
@@ -9,7 +9,6 @@ import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
